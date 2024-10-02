e6d7631af13dc62be536aeb2c37656ca21f4bc32
Sejun Park <sjunepark.com@gmail.com>
2024-10-02 11:29:20 +0900

    feat: Implement get_company_ids and upsert_companies for db::smes (#1)
    * feat: implement get_company_ids
    
    * feat: implement upsert_companies

--- crates/db/src/smes/db.rs
+++ crates/db/src/smes/db.rs
@@ -1,5 +1,6 @@
 use crate::smes::model::Params;
 use crate::{Company, DbError, LibsqlDb};
+use serde::Deserialize;
 
 impl LibsqlDb {
     #[tracing::instrument(skip(self))]
@@ -15,12 +16,30 @@ impl LibsqlDb {
         Ok(companies)
     }
 
+    #[tracing::instrument(skip(self))]
+    pub async fn get_company_ids(&self) -> Result<hashbrown::HashSet<String>, DbError> {
+        let mut rows = self.connection.query("SELECT id FROM Company", ()).await?;
+        let mut company_ids = hashbrown::HashSet::new();
+
+        #[derive(Deserialize)]
+        struct IdStruct {
+            id: String,
+        }
+
+        while let Some(row) = rows.next().await? {
+            let id_struct: IdStruct = libsql::de::from_row(&row)?;
+            company_ids.insert(id_struct.id);
+        }
+
+        Ok(company_ids)
+    }
+
     #[tracing::instrument(skip(self, companies))]
     pub async fn insert_companies(&self, companies: &[Company]) -> Result<(), DbError> {
         let tx = self.connection.transaction().await?;
         let mut stmt = tx
             .prepare(
-                "INSERT INTO Company (id,
+                "INSERT INTO company (id,
                      representative_name,
                      headquarters_address,
                      business_registration_number,
@@ -45,6 +64,43 @@ VALUES (:id,
 
         Ok(())
     }
+
+    #[tracing::instrument(skip(self, companies))]
+    pub async fn upsert_companies(&self, companies: &[Company]) -> Result<(), DbError> {
+        let tx = self.connection.transaction().await?;
+        let mut stmt = tx
+            .prepare(
+                "INSERT INTO company (id,
+                     representative_name,
+                     headquarters_address,
+                     business_registration_number,
+                     company_name,
+                     industry_code,
+                     industry_name)
+VALUES (:id,
+        :representative_name,
+        :headquarters_address,
+        :business_registration_number,
+        :company_name,
+        :industry_code,
+        :industry_name)
+ON CONFLICT (id) DO UPDATE SET representative_name          = EXCLUDED.representative_name,
+                               headquarters_address         = EXCLUDED.headquarters_address,
+                               business_registration_number = EXCLUDED.business_registration_number,
+                               company_name                 = EXCLUDED.company_name,
+                               industry_code                = EXCLUDED.industry_code,
+                               industry_name                = EXCLUDED.industry_name",
+            )
+            .await?;
+
+        for company in companies {
+            stmt.execute(company.params()).await?;
+            stmt.reset();
+        }
+        tx.commit().await?;
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -52,7 +108,7 @@ mod tests {
     use fake::Fake;
 
     use crate::test_utils::{text_context, DbSource, TestContext};
-    use crate::Company;
+    use crate::{Company, LibsqlDb};
 
     #[tokio::test]
     async fn insert_and_get_companies_should_work() {
@@ -60,8 +116,109 @@ mod tests {
 
         let ctx = text_context!(DbSource::Migration).await;
         let db = &ctx.db;
+        let companies = populate_companies(db, 10).await;
+
+        let db_companies = db
+            .get_companies()
+            .await
+            .inspect_err(|e| tracing::error!(?e, "Failed to get companies"))
+            .unwrap();
+
+        assert_eq!(db_companies.len(), companies.len());
+    }
+
+    #[tokio::test]
+    async fn insert_and_get_company_ids_should_work() {
+        tracing_setup::subscribe();
+
+        let ctx = text_context!(DbSource::Migration).await;
+        let db = &ctx.db;
+        let companies = populate_companies(db, 10).await;
+
+        let db_company_ids = db
+            .get_company_ids()
+            .await
+            .inspect_err(|e| tracing::error!(?e, "Failed to get company ids"))
+            .unwrap();
+
+        let company_ids: hashbrown::HashSet<String> = companies.into_iter().map(|c| c.id).collect();
+        tracing::trace!(?company_ids);
+        assert_eq!(db_company_ids, company_ids);
+    }
+
+    #[tokio::test]
+    async fn upsert_companies_should_work() {
+        // region: Arrange
+        tracing_setup::subscribe();
+
+        let ctx = text_context!(DbSource::Migration).await;
+        let db = &ctx.db;
+
+        // Set up basic companies
+        let companies = populate_companies(db, 10).await;
+
+        // Create companies to upsert: from existing companies.
+        const UPDATED_REPRESENTATIVE_NAME: &str = "Updated";
+        let mut updated_companies = companies
+            .iter()
+            .map(|c| Company {
+                representative_name: UPDATED_REPRESENTATIVE_NAME.to_string(),
+                ..c.clone()
+            })
+            .collect::<Vec<_>>();
+
+        // Add a new company to see that this company was properly updated
+        let mut new_company = ().fake::<Company>();
+        const NEW_COMPANY_ID: &str = "2000000000";
+        new_company.id = NEW_COMPANY_ID.to_string();
+        let new_company_representative_name = new_company.representative_name.clone();
+        updated_companies.push(new_company);
+
+        // Remove a company to check that this company was not updated
+        let removed_company = updated_companies.pop().unwrap();
+        let removed_company_id = removed_company.id.as_str();
+        // endregion: Arrange
+
+        // region: Action
+        db.upsert_companies(&updated_companies)
+            .await
+            .inspect_err(|e| tracing::error!(?e, "Failed to upsert companies"))
+            .unwrap();
+        // endregion: Action
+
+        // region: Assert
+        let db_companies = db
+            .get_companies()
+            .await
+            .inspect_err(|e| tracing::error!(?e, "Failed to get companies"))
+            .unwrap();
+
+        for company in &db_companies {
+            match company.id.as_str() {
+                NEW_COMPANY_ID => {
+                    assert_eq!(company.representative_name, new_company_representative_name);
+                }
+                id if id == removed_company_id => {
+                    // Not upserted company name should not change
+                    assert_eq!(
+                        company.representative_name,
+                        removed_company.representative_name
+                    );
+                }
+                _ => {
+                    assert_eq!(
+                        company.representative_name.as_str(),
+                        UPDATED_REPRESENTATIVE_NAME
+                    );
+                }
+            }
+        }
+        // endregion: Assert
+    }
+
+    async fn populate_companies(db: &LibsqlDb, size: usize) -> Vec<Company> {
         let mut incremental_id: usize = 1000000000;
-        let companies: Vec<Company> = (0..10)
+        let companies: Vec<Company> = (0..size)
             .map(|_| {
                 let company = ().fake::<Company>();
                 let id = incremental_id.to_string();
@@ -72,15 +229,11 @@ mod tests {
 
         db.insert_companies(&companies)
             .await
-            .inspect_err(|e| tracing::error!(?e, "Failed to insert companies"))
+            .inspect_err(|e| {
+                tracing::error!(?e, "Failed to insert companies");
+            })
             .unwrap();
 
-        let db_companies = db
-            .get_companies()
-            .await
-            .inspect_err(|e| tracing::error!(?e, "Failed to get companies"))
-            .unwrap();
-
-        assert_eq!(db_companies.len(), companies.len());
+        companies
     }
 }
